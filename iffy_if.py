1ec572092d5cdcf9cea65bc91b64863fb755815e
Andy Jakubowski <hello@andyjakubowski.com>
2020-09-28 11:23:25 +0200

    Tweak iffy_if.py

--- iffy_if.py
+++ iffy_if.py
@@ -1,8 +1,12 @@
-print("Hello", "World!", sep=', ', end=" Yahoo!!!\n")
-
 age = int(input("Gimme your age:\n"))
 
-if age < 30:
-    print("You are so young!")
+if age < 0:
+    print("Are you telling me you haven’t been born yet?")
+elif age == 0 or age == 1 or age == 2:
+    print("You’re such a baby.")
+elif age > 2 and age <= 30:
+    print("You are thirty or below.")
+elif age > 30 and age < 100:
+    print("You are over thirty, and under a hundred.")
 else:
     print("You are so wise!")
